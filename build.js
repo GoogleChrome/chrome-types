cd6ffbf719b967d4253b00632852759dedb81df7
Sam Thorogood <sam.thorogood@gmail.com>
2020-12-01 16:14:54 +1100

    apply patch

--- build.js
+++ build.js
@@ -41,6 +41,11 @@ const allPaths = [
   ...definitionPaths,
 ];
 
+const patchesToApply = [
+  // This is the patch out to create TSDoc types.
+  'https://chromium-review.googlesource.com/changes/chromium%2Fsrc~2544287/revisions/4/patch?download',
+];
+
 /**
  * Fetch the specified part of the Chromium source tree into the target work path.
  *
@@ -104,6 +109,37 @@ async function fetchAndPrepare(targetPath, chromePath) {
 }
 
 /**
+ * @param {string[]} args
+ * @param {string} cwd
+ * @param {string|undefined} stdin
+ * @return {!Promise<{code: number, out: Buffer}>}
+ */
+function exec(args, cwd, stdin=undefined) {
+  const command = args.shift();
+  return new Promise((resolve) => {
+    const ls = childProcess.spawn(command, args, {
+      cwd,
+      shell: true,
+    });
+    const chunks = [];
+    const errorChunks = [];
+
+    if (stdin !== undefined) {
+      ls.stdin.write(stdin);
+      ls.stdin.end();
+    }
+
+    ls.stdout.on('data', (data) => chunks.push(data));
+    ls.stderr.on('data', (data) => errorChunks.push(data));
+
+    ls.on('close', (code) => {
+      const out = Buffer.concat(code ? errorChunks : chunks);
+      return resolve({code, out});
+    });
+  });
+}
+
+/**
  * Run the types generation process.
  *
  * @param {string} target temporary folder to use (will be removed before start)
@@ -114,6 +150,15 @@ async function run(target) {
   await Promise.all(allPaths.map((chromePath) => {
     return fetchAndPrepare(target, chromePath);
   }));
+
+  // Apply patches.
+  for (const patchUrl of patchesToApply) {
+    const base64PatchBytes = await fetchAndCache(patchUrl);
+    const output = Buffer.from(base64PatchBytes.toString('utf-8'), 'base64').toString('utf-8');
+
+    const args = ['patch', '-s', '-p1'];
+    await exec(args, target, output);
+  }
 
   // Create a sorted list of source JSON/IDL files that can be processed by
   // this tool.
@@ -136,26 +181,12 @@ async function run(target) {
   // Sort by filename, not by directory.
   definitions.sort((a, b) => path.basename(a).localeCompare(path.basename(b)));
 
+  // Perform compilation in parallel.
+  // Testing on an iMac Pro brings this from 13s => 3s.
   const extracted = await tasks(definitions, async (definition) => {
-    // TODO(samthor): switch to tsdoc
-    const args = ['tools/json_schema_compiler/compiler.py', '-g', 'interface', definition];
-
-    return new Promise((resolve) => {
-      const ls = childProcess.spawn('python', args, {
-        cwd: target,
-        shell: true,
-      });
-      const chunks = [];
-      const errorChunks = [];
-
-      ls.stdout.on('data', (data) => chunks.push(data));
-      ls.stderr.on('data', (data) => errorChunks.push(data));
-
-      ls.on('close', (code) => {
-        const out = Buffer.concat(code ? errorChunks : chunks);
-        return resolve({definition, code, out});
-      });
-    });
+    const args = ['python', 'tools/json_schema_compiler/compiler.py', '-g', 'tsdoc', definition];
+    const {code, out} = await exec(args, target)
+    return {definition, code, out};
   });
 
   for (const {definition, code, out} of extracted) {
