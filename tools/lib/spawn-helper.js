6f061380c2bec37bdc1a4ee28e6bdefa6e5a0827
Sam Thorogood <sam.thorogood@gmail.com>
2021-10-13 12:18:54 +1100

    cleanup stuff

---
+++ tools/lib/spawn-helper.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import * as childProcess from 'child_process';
+
+
+/**
+ * @param {string[]} parts
+ * @param {{cwd?: string, input?: Buffer, stdoutInherit?: true}} options
+ */
+export default function run(parts, { cwd, input, stdoutInherit } = {}) {
+  /** @type {childProcess.SpawnSyncOptionsWithBufferEncoding} */
+  const options = {
+    stdio: ['pipe', stdoutInherit ? 'inherit' : 'pipe', 'inherit'],
+    maxBuffer: 1024 * 1024 * 16,  // 16mb
+    encoding: 'buffer',
+  };
+  if (cwd) {
+    options.cwd = cwd;
+  }
+  if (input) {
+    options.input = input;
+  }
+
+  const [command, ...rest] = parts;
+  const { stdout, status, error } = childProcess.spawnSync(command, rest, options);
+  if (error) {
+    throw error;
+  }
+  if (status) {
+    throw new Error(`code: ${status}`);
+  }
+  return stdout;
+}
--- tools/run-release.js
+++ tools/run-release.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import * as childProcess from 'child_process';
+import run from './lib/spawn-helper.js';
 import * as path from 'path';
 import * as fs from 'fs';
 import log from 'fancy-log';
@@ -11,55 +11,37 @@ const distDir = path.join(rootDir, 'dist');
 
 
 /**
- * @param {string[]} parts
- * @param {{input?: Buffer, stdoutInherit?: true}} options
- */
-function run(parts, { input, stdoutInherit } = {}) {
-  /** @type {childProcess.SpawnSyncOptionsWithBufferEncoding} */
-  const options = {
-    cwd: rootDir,
-    stdio: ['pipe', stdoutInherit ? 'inherit' : 'pipe', 'inherit'],
-    maxBuffer: 1024 * 1024 * 16,  // 16mb
-    encoding: 'buffer',
-  };
-  if (input) {
-    options.input = input;
-  }
-
-  const [command, ...rest] = parts;
-  const { stdout, status, error } = childProcess.spawnSync(command, rest, options);
-  if (error) {
-    throw error;
-  }
-  if (status) {
-    throw new Error(`code: ${status}`);
-  }
-  return stdout;
-}
-
-
-/**
  * @param {string} tool
  * @param {{input?: Buffer, args?: string[]}} options
  * @return {Buffer}
  */
-function runTool(tool, { input, args } = {}) {
+function toolInvoke(tool, { input, args } = {}) {
   const p = path.join('tools', tool);
-  return run([p, ...args ?? []], { input });
+  return run([p, ...args ?? []], { input, cwd: rootDir });
+}
+
+
+/**
+ * @param {string} p
+ */
+function typescriptCheck(p) {
+  run(['./node_modules/.bin/tsc', p], { stdoutInherit: true, cwd: rootDir });
 }
 
 
 log('Fetching Chrome payload...');
-const chromePayload = runTool('prepare.js');
+const chromePayload = toolInvoke('prepare.js');
 
 log('Building types for MV3+...');
-const indexTypesContent = runTool('render-tsd.js', { input: chromePayload });
-fs.writeFileSync(path.join(distDir, 'index.d.ts'), indexTypesContent);
-run(['./node_modules/.bin/tsc', 'dist/index.d.ts'], { stdoutInherit: true });
+const indexTypesContent = toolInvoke('render-tsd.js', { input: chromePayload });
+const indexTypesFile = path.join(distDir, 'index.d.ts');
+fs.writeFileSync(indexTypesFile, indexTypesContent);
+typescriptCheck(indexTypesFile);
 
 log('Building types for all...');
-const allTypesContent = runTool('render-tsd.js', { input: chromePayload, args: ['-a'] });
-fs.writeFileSync(path.join(distDir, '_all.d.ts'), allTypesContent);
-run(['./node_modules/.bin/tsc', 'dist/_all.d.ts'], { stdoutInherit: true });
+const allTypesContent = toolInvoke('render-tsd.js', { input: chromePayload, args: ['-a'] });
+const allTypesFile = path.join(distDir, '_all.d.ts');
+fs.writeFileSync(allTypesFile, allTypesContent);
+typescriptCheck(allTypesFile);
 
 log('Done!');
