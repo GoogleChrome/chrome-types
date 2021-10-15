edde55296a0e9ac41dc08fcefbcd19c4aa3c66d1
Sam Thorogood <sam.thorogood@gmail.com>
2021-10-15 14:23:26 +1100

    addListener returns void

--- content/preamble.d.ts
+++ content/preamble.d.ts
@@ -7,6 +7,6 @@ interface DOMFileSystem extends FileSystem {}
 
 // This is used to support addListener() where additional parameters are supported.
 // The name is detected inside the developer.chrome.com repository and special actions are taken.
-type InternalEventExtraParameters<H extends { addListener(callback: any, ...rest: any) }> =
+type InternalEventExtraParameters<H extends { addListener(callback: any, ...rest: any): void }> =
     Omit<chrome.events.Event<Parameters<H['addListener']>[0]>, 'addListener'> & Readonly<H>;
 
