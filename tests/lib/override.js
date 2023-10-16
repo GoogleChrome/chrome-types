3c3007a36167f892a20e4392563911dcef1fa32a
Oliver Dunk <oliverdunk@google.com>
2023-10-16 12:06:26 +0100

    Add tests

--- tests/lib/override.js
+++ tests/lib/override.js
@@ -13,3 +13,22 @@ test('setPanelBehavior is visible', t => {
 
   t.assert(rc.isVisible({ nodoc: true }, 'api:sidePanel.setPanelBehavior'));
 });
+
+test('chrome-install-location tag is added', (t) => {
+  const rc = new RenderOverride({}, new FeatureQuery({'api:sidePanel.setPanelBehavior': { location: 'policy' }}), {
+    generated: "",
+    high: 2,
+    low: 1,
+    revision: 0,
+    symbols: {
+      'api:sidePanel.setPanelBehavior': {
+        high: 2
+      }
+    }
+  });
+
+  t.deepEqual(
+    rc.completeTagsFor({ id: 'api:sidePanel.setPanelBehavior' }, 'api:sidePanel.setPanelBehavior'),
+    [{ name: 'chrome-install-location', value: 'policy' }]
+  );
+});
